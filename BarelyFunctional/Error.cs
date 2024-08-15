6697f1c8503bd67c8edcd00fc2870e32d995e6ba
Eivind S. Antonsen <esa@telia.no>
2024-08-15 13:31:49 +0200

    Fixed typo Refactor Error class and update Outcome handling
    Refactored the `Error` class to use `IEnumerable<Error>` for the `_errors` field and related parameters, enhancing flexibility. Added new properties `IsExceptional`, `Exception`, and `Message` to the `Error` class. Introduced static methods `FromException`, `FromMessage`, and `FromMany` for creating `Error` instances. Added implicit conversion operators for `string` and `Exception` to `Error`.
    
    Renamed the implicit conversion operator parameter in `Outcome<T>` from `t` to `value` and updated `catch` blocks to use `Failure<TResult>(exception)` directly.
    
    Moved tests from `OutcomeTests.cs` to a new file `ErrorTests.cs`, including tests for the new `Error` methods with `Assert.Multiple`.

--- BarelyFunctional/Error.cs
+++ BarelyFunctional/Error.cs
@@ -1,37 +1,48 @@
 ﻿using System.Net;
+// ReSharper disable MemberCanBePrivate.Global
 
 namespace BarelyFunctional;
 
 public record Error
 {
-    private Error(Exception? exception = null, string? errorMessage = null, List<Error>? errors = null)
+    private Error(Exception? exception = null, string? errorMessage = null, IEnumerable<Error>? errors = null)
     {
         Exception = exception;
         Message = errorMessage;
         _errors = errors ?? [];
     }
 
+
     private readonly IEnumerable<Error> _errors;
+
 
     public bool IsExceptional =>
         Exception is not null || Errors.Any(error => error.IsExceptional) == true;
 
+
     public Exception? Exception { get; }
+
     public string? Message { get; }
-    public IEnumerable<Error> Errors => _errors ?? [this];
+
+    public IEnumerable<Error> Errors => _errors ?? [];
+
 
     public static Error FromException(Exception exception) =>
         new(exception);
 
+
     public static Error FromMessage(string errorMessage) =>
         new(default, errorMessage);
 
-    public static Error FromMany(IEnumerable<Error> errors) =>
-        new(default, null, errors.ToList());
 
-    public static implicit operator Error(string message) => 
+    public static Error FromMany(IEnumerable<Error> errors) =>
+        new(default, null, errors);
+
+
+    public static implicit operator Error(string message) =>
         FromMessage(message);
 
-    public static implicit operator Error(Exception exception) => 
+
+    public static implicit operator Error(Exception exception) =>
         FromException(exception);
 }
