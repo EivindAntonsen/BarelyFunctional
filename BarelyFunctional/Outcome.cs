ceee91018ba75ca9a8fe64270812850683a2d823
Eivind Antonsen <eivind.antonsen@gmail.com>
2024-08-30 10:14:31 +0200

    Added some documentation

--- BarelyFunctional/Outcome.cs
+++ BarelyFunctional/Outcome.cs
@@ -263,8 +263,18 @@ public static class OutcomeExtensions
             ? Outcome<Unit>.Success()
             : Outcome<Unit>.Failure(outcome.Error!);
 
-
-    public static Outcome<IEnumerable<T>> Sequence<T>(this IEnumerable<Outcome<T>> collection)
+    /// <summary>
+    /// Takes a collection of outcomes and returns an outcome that represents the result of the sequence operation.
+    /// </summary>
+    /// <typeparam name="T">The type of each outcome in the input collection.</typeparam>
+    /// <param name="collection">The input collection of outcomes.</param>
+    /// <returns>
+    /// An outcome that represents the result of the sequence operation.
+    /// If any outcome in the collection is a failure, the resulting outcome will also be a failure with the corresponding errors.
+    /// If all outcomes in the collection are successes, the resulting outcome will be a success with a collection of the values.
+    /// </returns>
+    public static Outcome<IEnumerable<T>> Sequence<T>(
+        this IEnumerable<Outcome<T>> collection)
     {
         var list = collection.ToList();
 
@@ -279,6 +289,18 @@ public static class OutcomeExtensions
     }
 
 
+    /// <summary>
+    /// Traverses a collection and applies a function to each element, returning an outcome that represents the result of the traversal.
+    /// </summary>
+    /// <typeparam name="TOutput">The type of the elements in the resulting collection.</typeparam>
+    /// <typeparam name="TInput">The type of the elements in the input collection.</typeparam>
+    /// <param name="collection">The input collection to traverse.</param>
+    /// <param name="func">The function to apply to each element in the collection.</param>
+    /// <returns>
+    /// An outcome that represents the result of the traversal.
+    /// If all elements in the collection were successfully transformed, the outcome will contain a collection of the transformed values.
+    /// If any element fails the transformation, the outcome will contain an error indicating the failure.
+    /// </returns>
     public static Outcome<IEnumerable<TOutput>> Traverse<TOutput, TInput>(
         this IEnumerable<TInput> collection,
         Func<TInput, Outcome<TOutput>> func)
--- readme.md
+++ readme.md
@@ -3,11 +3,27 @@
 ## What is this?
 This is a minimal functional library for C# that provides a single monadic container for your day-to-day functional basics. It is designed to be as simple as possible, with a single monadic container that can be used to chain operations together in a functional way.
 
-## What is Outcome<T>?
-Outcome<T> is a monadic container that can hold either a value of type T or an error message. It is designed to be used in a functional way, allowing you to chain operations together in a way that is easy to understand and use.
+## What is `Outcome<T>`?
+Outcome is a monadic container that can hold either a value of type T or an error message. 
+It is designed to be used in a functional way, allowing you to chain operations together in a way that is easy to understand and use.
 
-The Outcome will be in either a Success or Failure state. If it is in a Success state, it will contain a value of type T. If it is in a Failure state, it will contain an error.
-Methods can be chained with methods like `Select()`, `SelectMany()`, `Traverse()`, and any errors occurring will shortcircuit the chain and return an Outcome in a failure state with the error information.
+## Usage
+````csharp
+List<string> numbers = new List<string> { "7", "2", "4", "9", "8", "3", "1" };
+
+HttpResponse response = numbers
+    // Wrap the parsing of each number in their own Outcome<int>
+    .Select(number => Outcome<int>.Of(() => int.Parse(number)))
+    // Invert the IEnumerable<Outcome<int>> structure into the much easier
+    // Outcome<IEnumerable<int>> structure with Traverse.
+    .Traverse(i => i)
+    // Filter the successful outcome with a predicate.
+    .Where(ints => ints.Any(int.IsEvenInteger))
+    // Handle both success and error cases with Match.
+    .Match(success => HttpResponse.Ok(success),
+           error   => HttpResponse.NotFound(error.Message));
+````
+
 
 ## Installation
 The project exists on nuget: https://www.nuget.org/packages/BarelyFunctional
